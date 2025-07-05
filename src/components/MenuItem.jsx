5d191a48f27df1c9c6b515ee20494e6d24cd5388
Favio Cabral <faviocabral@gmail.com>
2025-07-05 15:21:40 -0300

    update menu item

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../App.css'
 export default function MenuItem({menuName, icono }) {
-
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    } 
     const menuLista = ['Menu 1', 'Menu 2', 'Menu 3']
     /**
      * para listar los menu de un modulo 
@@ -16,11 +19,15 @@ export default function MenuItem({menuName, icono }) {
                 </a>
             </li>
         })
-    }
+    } 
 
     return (
     <>
-              <li className="nav-item" style={{marginLeft:'0px'}}>
+              <li 
+                className= {isOpen? 'nav-item menu-is-opening menu-open':'nav-item'} 
+                style={{marginLeft:'0px'}}
+                onClick={toggleMenu}
+              >
                 <a href="#" className="nav-link active">
                   <i className= {`nav-icon pi ${icono}`}  />
                   <p>
