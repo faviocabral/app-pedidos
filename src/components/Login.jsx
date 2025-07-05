a4902797b024b1f6adab1ecfd7d3dc188482a8e6
Favio Cabral <faviocabral@gmail.com>
2025-07-05 15:29:20 -0300

    update login position

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
   return (
     <>
     {/* <div className="login-container d-flex justify-content-center  align-items-center h-100" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover' }} > */}
-    <div className="login-container d-flex justify-content-center  align-items-center h-100" >
+    <div className="login-container d-flex justify-content-center  align-items-center" style={{ minHeight: '100vh' }} >
     <div className="login-box" style={{opacity:"0.8"}} >
             <h1 className="text-center text-dark text-bold">SISTEMA PEDIDOS</h1>
             <div className="card" id="myLogin">
